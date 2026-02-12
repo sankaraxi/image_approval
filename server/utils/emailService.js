da181f901709373cc1b9e4aa52dddae8b157f1c4
Sankar K G <sankarfrompalani@gmail.com>
2026-02-12 16:39:00 +0530

    fix: update email recipient for task creation and daily report notifications

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -21,7 +21,7 @@ const sendTaskCreationEmail = async (taskDetails) => {
     
     const mailOptions = {
       from: 'info@kggeniuslabs.com',
-      to: 'sankar.k@kggeniuslabs.com',
+      to: 'krishnapriya.p@kggeniuslabs.com',
       cc: 'info@kggeniuslabs.com',
       subject: `New Task Created: ${taskDetails.title}`,
       html: `
@@ -62,7 +62,7 @@ const sendDailyReportEmail = async (stats) => {
     
     const mailOptions = {
       from: 'info@kggeniuslabs.com',
-      to: 'sankar.k@kggeniuslabs.com',
+      to: 'krishnapriya.p@kggeniuslabs.com',
       cc: 'info@kggeniuslabs.com',
       subject: `Daily Report - Image Scanner Statistics (${new Date().toLocaleDateString()})`,
       html: `
